0f335e3813cb047653b67d21dd7244a4db0e8a3a
Alex Doru <alexandru.doru@unifiedpost.com>
2018-05-11 23:33:25 +0300

    changes from api to mvc

--- spec/services/here_maps/routes_repo_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-describe HereMaps::RoutesRepo do
-
-  it 'calls the api' do
-    xml = HereMaps::RoutesRepo.new.calculateroute('departure', 'destination')
-    expect(xml).to be_a(String)
-  end
-end
