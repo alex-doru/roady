7d9364b085a7f7444f10916a0e199c0a3610c18c
Vlad Velciov <velciov.vlad@gmail.com>
2018-05-11 19:47:55 +0300

    Service for accessing here maps apis

---
+++ app/controllers/locations_controller_controller.rb
@@ -0,0 +1,3 @@
+class LocationsControllerController < ApplicationController
+
+end
---
+++ app/services/here_maps_service.rb
@@ -0,0 +1,15 @@
+class HereMapsService
+
+
+  def location_coordinates(location_name)
+
+  end
+
+  def route_link_ids(departure, destination)
+
+  end
+
+  def tolls(link_ids)
+
+  end
+end
